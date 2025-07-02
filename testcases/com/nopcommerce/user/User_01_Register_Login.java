7a9c20769a9e9526c60378c2d60b46150fd05e3f
Doan Van <90561124+doanvaz@users.noreply.github.com>
2025-07-02 17:38:33 +0700

    Init automation framework

--- testcases/com/nopcommerce/user/User_01_Register_Login.java
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -5,13 +5,17 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
 
 public class User_01_Register_Login {
-
-	@Test
-	public void f() {
-	}
-
 	@BeforeClass
 	public void beforeClass() {
+	}
+
+	@Test
+	public void TC_01() {		
+	}
+	
+	@Test
+	public void TC_02() {	
+		System.out.print("hello");
 	}
 
 	@AfterClass
