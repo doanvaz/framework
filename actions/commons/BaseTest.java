afeb9cb8a6526270ae77879d52a892494fbc558c
Doan Van <90561124+doanvaz@users.noreply.github.com>
2025-07-23 21:41:26 +0700

    Add WebDriverManager libraries

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -5,20 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 	private WebDriver driver;
 	private String projectPath = System.getProperty("user.dir");
+
 	protected WebDriver getBrowserDriver(String browserName) {
 		if (browserName.equals("chrome")) {
-			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-		}
-		else if (browserName.equals("edge")) {
-			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+		} else if (browserName.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath+"\\BrowserDrivers\\msedgedriver.exe");
 			driver = new EdgeDriver();
-		}
-		else if (browserName.equals("firefox")) {
-			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		} else if (browserName.equals("firefox")) {
+
+			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		} else {
 			throw new RuntimeException("Browser name is invalid!");
